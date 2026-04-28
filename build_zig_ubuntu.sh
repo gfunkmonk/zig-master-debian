27f9c2cd59ae6789b0a1929d27b3d9e8121bfdc4
Matt Rinsch <gfunkmonk@mailbox.org>
2026-04-28 04:42:04 -0400

    Fix docker build command argument order

--- build_zig_ubuntu.sh
+++ build_zig_ubuntu.sh
@@ -5,7 +5,7 @@ for i in "${arr[@]}"
 do
   DEBIAN_DIST=$i
   FULL_VERSION=$ZIG_VERSION-${BUILD_VERSION}+${DEBIAN_DIST}_amd64
-  docker build . -t -f Dockerfile.ubuntu zig-master-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION
+  docker build . -f Dockerfile.ubuntu -t zig-master-$DEBIAN_DIST --build-arg ZIG_VERSION=$ZIG_VERSION --build-arg DEBIAN_DIST=$DEBIAN_DIST --build-arg BUILD_VERSION=$BUILD_VERSION --build-arg FULL_VERSION=$FULL_VERSION
   id="$(docker create zig-master-$DEBIAN_DIST)"
   docker cp $id:/zig-master_$FULL_VERSION.deb - > ./zig-master_$FULL_VERSION.deb
   tar -xf ./zig-master_$FULL_VERSION.deb
